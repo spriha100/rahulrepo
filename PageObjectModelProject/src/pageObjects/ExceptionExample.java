8497502545168d7481553dbb7eeca94edad98fea
SHINJINI BANERJEE <SHINJINI BANERJEE@hp>
2019-02-20 23:51:00 +0530

    commit

---
+++ PageObjectModelProject/src/pageObjects/ExceptionExample.java
@@ -0,0 +1,15 @@
+package pageObjects;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) 
+	{		
+		System.out.println("First line ");
+	    int c = 5/0;		
+		System.out.println(c);	//third line
+		System.out.println("Forth line");//forth line
+		
+
+	}
+
+}
